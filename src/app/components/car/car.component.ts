8170518b4171cd211791a324298f77392cd8ef35
Kübra Bol <kubra1272@icloud.com>
2023-09-05 00:45:42 +0300

    Data List

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/models/car';
+import {HttpClient} from '@angular/common/http';
+import { CarResponseModel } from 'src/app/models/carResponseModel';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
-export class CarComponent {
-
-  
+export class CarComponent implements OnInit{
 
 cars : Car [] =[];
-  
-constructor () {}
-ngOnInit():void{}
+apiUrl = "https://localhost:44388/api/cars/getall"
+carResponseModel : CarResponseModel={
+  data : this.cars,
+  messgae : " ",
+  succes : true
+};
+constructor (private httpClinet:HttpClient) {}
+
+ngOnInit():void{
+  this.getCars();
+}
+
+getCars() {
+  this.httpClinet.get<CarResponseModel>(this.apiUrl).subscribe((response) => {
+    this.cars=response.data
+
+  });
+}
+
+
 }
